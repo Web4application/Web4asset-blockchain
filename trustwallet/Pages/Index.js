54212318d2c1db81c5991eb8019b6b7ba7a10704
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-14 04:52:22 -0700

    Create Index.js

---
+++ trustwallet/Pages/Index.js
@@ -0,0 +1,10 @@
+import TrustWalletIntegration from '../components/TrustWalletIntegration';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Web4asset</h1>
+      <TrustWalletIntegration />
+    </div>
+  );
+}
