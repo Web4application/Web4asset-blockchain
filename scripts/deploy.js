241ed90625b7fb113582970cb7fa982ccd4d1258
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-18 15:48:55 -0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,13 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const Token = await ethers.getContractFactory("TestToken");
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deploying contracts with: ${deployer.address}`);
+
+  const Token = await ethers.getContractFactory("Web4AssetToken");
   const token = await Token.deploy();
   await token.deployed();
-  console.log("Token deployed to:", token.address);
+  console.log(`Token deployed at: ${token.address}`);
 }
 
 main().catch((error) => {
