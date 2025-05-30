84151de281b0cb2c12efea81cc76c93c0df569d6
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-30 09:24:01 +0200

    Update 1_deploy_contracts.js

--- migrate/1_deploy_contracts.js
+++ migrate/1_deploy_contracts.js
@@ -1,12 +1,13 @@
-// migrations/1_deploy_contracts.js
 const Oracle = artifacts.require("Oracle");
 const SupplyChainAutomation = artifacts.require("SupplyChainAutomation");
 
-module.exports = async function(deployer) {
-    // Deploy the Oracle contract first
-    await deployer.deploy(Oracle, { from: web3.eth.accounts[0] });
+module.exports = async function(deployer, network, accounts) {
+    const deployerAccount = accounts[0];
+
+    // Deploy Oracle contract
+    await deployer.deploy(Oracle, { from: deployerAccount });
     const oracle = await Oracle.deployed();
 
-    // Deploy the SupplyChainAutomation contract with the Oracle address
-    await deployer.deploy(SupplyChainAutomation, oracle.address, { from: web3.eth.accounts[0] });
+    // Deploy SupplyChainAutomation with Oracle's address
+    await deployer.deploy(SupplyChainAutomation, oracle.address, { from: deployerAccount });
 };
