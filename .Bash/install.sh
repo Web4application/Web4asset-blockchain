76c39c0f8938cc2b0060bec2a9d1da9048cc1b35
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-30 07:40:45 +0200

    Update install.sh

--- .Bash/install.sh
+++ .Bash/install.sh
@@ -1,2 +1,3 @@
 npm install ethers crypto react react-dom babel-loader @babel/core @babel/preset-react webpack webpack-cli webpack-dev-server
 npm install --save-dev hardhat
+solc --abi Web4asset.sol -o build
